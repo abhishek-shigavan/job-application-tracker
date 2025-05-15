7b7648214754c465144055703296eacbed29a042
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2025-05-16 00:37:02 +0530

    MODIFIED: updated UI & storing user info for dashboard

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,44 @@
 import { auth, provider } from '../../firebase'
 import { useNavigate } from "react-router-dom"
 import { signInWithPopup } from 'firebase/auth'
-import "Login.scss"
+import Button from '@mui/material/Button'
+import Snackbar from '@mui/material/Snackbar'
+import "./Login.scss"
+import { useState } from 'react'
 
 function Login () {
     const navigate = useNavigate()
+    const [showErrMsg, setShowErrMsg] = useState(false)
 
     const handleLogin = async () => {
         try {
-            await signInWithPopup(auth, provider)        
+            const loggedUser = await signInWithPopup(auth, provider)
+            const {displayName, email, photoURL} = loggedUser?.user || {displayName: "", email: "", photoURL: ""}
+            localStorage.setItem("name", displayName)
+            localStorage.setItem("email", email)
+            localStorage.setItem("profile", photoURL)
             navigate('/dashboard')
         } catch (err) {
-            console.error('Login failed:', err)
+            setShowErrMsg(true)
         }   
     }
 
     return (
         <div className='login-main-cnt'>
-            <h2>Login</h2>
-            <button onClick={handleLogin}>Sign in with Google</button>
+            <div className="login-action-btn-banner-cnt">
+                <div className="login-logo-btn-cnt">
+                    <span>Best way to track your job applications</span>
+                    <Button variant="text" sx={{width: "fit-content !important", padding: "5px 10px !important"}} onClick={handleLogin}>Sign in with Google</Button>
+                </div>
+                <div className="login-banner-cnt">
+                    <img src="./src/assets/jobtracklogo.png" alt="app baner"/>
+                </div>
+            </div>
+            <Snackbar
+                open={showErrMsg}
+                autoHideDuration={6000}
+                message="Login Failed...!"
+            />
         </div>
     )
 }
