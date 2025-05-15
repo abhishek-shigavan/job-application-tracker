5054a87e35c2193fede35933bc67c2a545fee248
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2025-05-15 13:47:47 +0530

    MODIFIED: displaying jobs in separate container

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,6 +4,8 @@ import Modal from '@mui/material/Modal'
 import { collection, query, where, orderBy, onSnapshot } from 'firebase/firestore'
 import { db } from "../../firebase"
 import { useAuth } from "../../useAuth"
+import JobCard from "../JobCard/JobCard"
+import "./Dashboard.scss"
 
 function Dashboard () {
     const { user } = useAuth()
@@ -37,9 +39,26 @@ function Dashboard () {
     const handleModalClose = () => setOpenAddJob(false)
 
     return (
-        <>
+        <div className="dashboard-main-cnt">
             <h1>Dashboard</h1>
             <button onClick={handleModalOpen}>Add job</button>
+            <div className="dashboard-jobs-pannel-cnt">
+                <div className="dashboard-applied-jobs-cnt">
+                    {jobsList.filter(job => job.status === "Applied").map(job => <JobCard key={job?.id} jobDetails={job}/>)}
+                </div>
+                <div className="dashboard-interview-jobs-cnt">
+                    {jobsList.filter(job => job.status === "Interview").map(job => <JobCard key={job?.id} jobDetails={job}/>)}
+                </div>
+                <div className="dashboard-offer-jobs-cnt">
+                    {jobsList.filter(job => job.status === "Offer").map(job => <JobCard key={job?.id} jobDetails={job}/>)}
+                </div>
+                <div className="dashboard-hired-jobs-cnt">
+                    {jobsList.filter(job => job.status === "Hired").map(job => <JobCard key={job?.id} jobDetails={job}/>)}
+                </div>
+                <div className="dashboard-rejected-jobs-cnt">
+                    {jobsList.filter(job => job.status === "Rejected").map(job => <JobCard key={job?.id} jobDetails={job}/>)}
+                </div>
+            </div>
             <Modal
                 open={openAddJob}
                 onClose={handleModalClose}
@@ -48,7 +67,7 @@ function Dashboard () {
             >
                 <AddJob closeAddJob={handleModalClose} />
             </Modal>
-        </>
+        </div>
     )
 }
 
