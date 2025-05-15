78414817875a387ae7bd788c92760630afb59de3
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2025-05-15 22:47:21 +0530

    MODIFIED: added listener for DND & details added

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,17 +1,33 @@
 import { useNavigate } from "react-router-dom"
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import Button from '@mui/material/Button'
 import "./JobCard.scss"
 
-function JobCard ({jobDetails}) {
+function JobCard ({ jobDetails }) {
+    const jobCreatedAt = new Date(jobDetails?.createdAt?.seconds * 1000).toLocaleDateString()
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: jobDetails.id })
+    
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition
+    }
+
     const navigate = useNavigate()
 
     const handleJobCard = () => {
         navigate(`/job/${jobDetails.id}`)
     }
-    
+
     return (
-        <div className="job-card-cnt" onClick={handleJobCard}>
-            <span>{jobDetails?.title}</span>
-            <span>{jobDetails?.status}</span>
+        <div ref={setNodeRef} style={style} className="job-card-cnt">
+            <div {...attributes} {...listeners} className="job-card-details-cnt">
+                <span>{jobDetails?.title}</span>
+                <span>{jobDetails?.company}</span>
+                <span>{jobDetails?.status}</span>
+                <span>Applied at {jobCreatedAt}</span>
+            </div>
+            <Button variant="outlined" sx={{width: "fit-content !important", padding: "5px 20px !important"}} onClick={handleJobCard}>See Details</Button>
         </div>
     )
 }
