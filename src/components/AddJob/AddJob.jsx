90642768b9c85cff0333d9247deea1455b3f45d8
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2025-05-15 20:52:59 +0530

    MODIFIED: added fields to get job data

--- src/components/AddJob/AddJob.jsx
+++ src/components/AddJob/AddJob.jsx
@@ -1,37 +1,74 @@
 import { addDoc, collection, serverTimestamp } from "firebase/firestore"
 import { db } from "../../firebase"
 import { useAuth } from "../../useAuth"
+import TextField from '@mui/material/TextField'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import Button from '@mui/material/Button'
+import { useState } from "react"
+import "./AddJob.scss"
 
 function AddJob ({closeAddJob}) {
     const { user } = useAuth()
+    const [jobDetails, setJobDetails] = useState({
+        title: "",
+        company: "",
+        status: "Applied",
+        notes: "",
+        logoURL: ''
+    })
+
+    const handleJobDetails = (updatedData) => setJobDetails({...jobDetails, ...updatedData})
+
+    const handleStatus = (e) => setJobDetails({...jobDetails, status: e.target.value })
 
     const handleAddJob = async() => {
-        const formData = {
-            title: 'Frontend Developer',
-            company: 'Zxxzv',
-            logoURL: '',
-            status: 'Applied',
-            notes: ''
-        }
-        
         try {
             await addDoc(collection(db, 'jobs'), {
-              ...formData,
-              owner: user.uid,
-              createdAt: serverTimestamp()
+                ...jobDetails,
+                owner: user.uid,
+                createdAt: serverTimestamp()
             })
         } catch (error) {
             console.error('Error adding job:', error)
         }
-
         closeAddJob()
     }
 
     return (
-        <>
-            <h1>AddJob</h1>
-            <button onClick={handleAddJob}>Save</button>
-        </>
+        <div className="add-job-inner-cnt">
+            <TextField
+                label="Role"
+                variant="outlined"
+                fullWidth
+                onChange={(e) => handleJobDetails({role: e.target.value})}
+            />
+            <TextField
+                label="Company"
+                variant="outlined"
+                fullWidth
+                onChange={(e) => handleJobDetails({company: e.target.value})}
+            />
+            <Select
+                fullWidth
+                value={jobDetails.status}
+                onChange={handleStatus}
+            >
+                <MenuItem value={"Applied"}>Applied</MenuItem>
+                <MenuItem value={"Interview"}>Interview</MenuItem>
+                <MenuItem value={"Offer"}>Offer</MenuItem>
+                <MenuItem value={"Hired"}>Hired</MenuItem>
+                <MenuItem value={"Rejected"}>Rejected</MenuItem>
+            </Select>
+            <TextField
+                label="Notes"
+                variant="outlined"
+                fullWidth
+                multiline={4}
+                onChange={(e) => handleJobDetails({notes: e.target.value})}
+            />
+            <Button variant="outlined" sx={{width: "fit-content !important", padding: "10px 20px !important"}} onClick={handleAddJob}>Save</Button>
+        </div>
     )
 }
 
